da646484052156705ba45a8a6f5d88d0965585e8
Glitch (omniscient-sun-soul) <none>
2020-09-04 16:56:20 +0000

    👗🎢 Checkpoint ./server.js:21184821/74

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get("/api/exercise/users",(req,res)=>{
 })
 
 app.post('/api/exercise/add',(req,res)=>{
-    User.findByIdAndUpdate(req.body.userId,{game:"rugby"},{new:true},(err,result)=>{
+    User.findByIdAndUpdate(req.body.userId,{$set{"game":"rugby"},(err,result)=>{
       res.json(result)
     })
 })
